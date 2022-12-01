6f3bd4c3688873f771b367b3775b439cbdb987a4
Sebastiaan Zaaijer <sebastiaan.zaaijer@xs4all.nl>
2022-12-01 09:26:47 +0000

    day 1 solutions

---
+++ day1/test_part2.py
@@ -0,0 +1,22 @@
+import unittest
+
+from .part2 import solve_puzzle
+
+class TestPuzzle(unittest.TestCase):
+    def test_example_input(self):
+        example_input = \
+"""1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+        self.assertEqual(solve_puzzle(example_input),45000)
