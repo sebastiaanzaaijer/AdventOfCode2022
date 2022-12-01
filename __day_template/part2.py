5d1aa2030362fbafb5f7ec28386d158e9a119b15
Sebastiaan Zaaijer <sebastiaan.zaaijer@xs4all.nl>
2022-12-01 09:26:35 +0000

    getting sorted

---
+++ __day_template/part2.py
@@ -0,0 +1,9 @@
+def solve_puzzle(puzzle_input):
+    return list(sum(map(int,block.splitlines())) for block in puzzle_input.split("\n\n"))
+
+if __name__ == "__main__":
+    import os
+    import pathlib
+    puzzle_path = pathlib.Path(__file__).parent.absolute()
+    puzzle_input = open(os.path.join(puzzle_path,"puzzle_input")).read()
+    print(solve_puzzle(puzzle_input))
