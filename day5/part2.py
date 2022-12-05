71f4627577d8182956d2a426b1d8c5e86d1303d2
Sebastiaan Zaaijer <sebastiaan.zaaijer@xs4all.nl>
2022-12-05 16:05:15 +0000

    day 5 solutions

---
+++ day5/part2.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+def make_move(stacks,nr,from_,to_):
+    crane = []
+    for i in range(nr):
+        crane.append(stacks[from_].pop())
+    for crate in crane[::-1]:
+        stacks[to_].append(crate)
+
+
+def solve_puzzle(puzzle_input):
+    lines = puzzle_input.splitlines()
+    stacks = defaultdict(list)
+    moves = []
+    while True:
+        l = lines.pop(0)
+        for i,chr in enumerate(l[1::4],1):
+            if chr != " ":
+                stacks[i].append(chr)
+        if l.strip().startswith("1"): break
+    for stack in stacks.values():
+        stack.reverse()
+    lines.pop(0)
+    for l in lines:
+        nr, from_, to_ = map(int,l.replace("move","").replace("from","").replace("to","").split())
+        make_move(stacks,nr,from_,to_)
+    return "".join(stacks[k][-1] for k in sorted(stacks))
+
+if __name__ == "__main__":
+    import os
+    import pathlib
+    puzzle_path = pathlib.Path(__file__).parent.absolute()
+    puzzle_input = open(os.path.join(puzzle_path,"puzzle_input")).read()
+    print(solve_puzzle(puzzle_input))
---
+++ day5/test_part1.py
@@ -0,0 +1,17 @@
+import unittest
+
+from .part1 import solve_puzzle
+
+class TestPuzzle(unittest.TestCase):
+    def test_example_input(self):
+        example_input = \
+"""    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"""
+        self.assertEqual(solve_puzzle(example_input),"CMZ")
